99001970639efec614aa062fc5fac55ab58ee022
MaratBagautdinov <marat.bagautdinov522@mail.ru>
2025-04-04 16:28:30 +0300

    update configs

--- src/createAdmin.js
+++ src/createAdmin.js
@@ -1,22 +1,22 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function (o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
+        desc = { enumerable: true, get: function () { return m[k]; } };
     }
     Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
+}) : (function (o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function (o, v) {
     Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
+}) : function (o, v) {
     o["default"] = v;
 });
 var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
+    var ownKeys = function (o) {
         ownKeys = Object.getOwnPropertyNames || function (o) {
             var ar = [];
             for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
@@ -45,8 +45,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const User_1 = require("./src/models/User");
-const init_1 = require("./src/db/init");
+const User_1 = require("./models/User");
+const init_1 = require("./db/init");
 const bcrypt_1 = __importDefault(require("bcrypt"));
 function createAdmin() {
     return __awaiter(this, void 0, void 0, function* () {
@@ -64,7 +64,7 @@ function createAdmin() {
             if (existingUser.role !== 'admin') {
                 // Примечание: В текущей модели нет метода обновления роли,
                 // поэтому используем прямой SQL-запрос
-                const db = (yield Promise.resolve().then(() => __importStar(require('./src/db/init')))).default;
+                const db = (yield Promise.resolve().then(() => __importStar(require('./db/init')))).default;
                 yield new Promise((resolve, reject) => {
                     db.run('UPDATE users SET role = ? WHERE email = ?', ['admin', email], function (err) {
                         if (err) {
--- src/createAdmin.ts
+++ src/createAdmin.ts
@@ -1,33 +1,33 @@
-import { UserModel } from './src/models/User';
-import { initializeDatabase } from './src/db/init';
+import { UserModel } from './models/User';
+import { initializeDatabase } from './db/init';
 import bcrypt from 'bcrypt';
 
 async function createAdmin() {
   await initializeDatabase();
-  
+
   const email = 'admin';
   const password = '17072020';
-  
+
   // Хешируем пароль
   const hashedPassword = await bcrypt.hash(password, 10);
-  
+
   const existingUser = await UserModel.findByEmail(email);
-  
+
   if (existingUser) {
     // Обновляем существующего пользователя
     await UserModel.confirmEmail(email);
     await UserModel.updatePassword(existingUser.id as number, password);
-    
+
     // Устанавливаем роль администратора (если не установлена)
     if (existingUser.role !== 'admin') {
       // Примечание: В текущей модели нет метода обновления роли,
       // поэтому используем прямой SQL-запрос
-      const db = (await import('./src/db/init')).default;
+      const db = (await import('./db/init')).default;
       await new Promise<void>((resolve, reject) => {
         db.run(
           'UPDATE users SET role = ? WHERE email = ?',
           ['admin', email],
-          function(err) {
+          function (err) {
             if (err) {
               reject(err);
             } else {
@@ -37,7 +37,7 @@ async function createAdmin() {
         );
       });
     }
-    
+
     console.log('Администратор уже существует. Учетные данные обновлены.');
   } else {
     // Создаем нового администратора
@@ -49,7 +49,7 @@ async function createAdmin() {
     });
     console.log('Администратор создан успешно.');
   }
-  
+
   console.log('Данные для входа в админ-панель:');
   console.log('Логин: admin');
   console.log('Пароль: 17072020');
